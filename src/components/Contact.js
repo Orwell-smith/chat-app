afb84757aa7aef25d98a3fa1ec9e46e7b515e5a0
Léonard Fonteneau <leonardfonteneau@gmail.com>
2019-10-22 09:39:58 +0200

    finish-state2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -33,7 +33,7 @@ class Contact extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            online: false
+            online: props.online
         }
     }
 
